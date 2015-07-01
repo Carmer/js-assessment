32442941e127fc385b06b0f9f8bc4815e7034f71
Andrew Carmer <carmerandrew@gmail.com>
2015-07-01 10:41:23 -0600

    flowcontrol

--- app/count.js
+++ app/count.js
@@ -1,7 +1,22 @@
 exports = (typeof window === 'undefined') ? global : window;
-
+// I got this from the answer sheet. still a little fuzzy
 exports.countAnswers =  {
   count : function (start, end) {
+    var timeout;
+    function doIt () {
+      console.log(start++);
 
+      if (start <= end) {
+        timeout = setTimeout(doIt, 100);
+      }
+    }
+
+    doIt();
+
+    return {
+      cancel : function () {
+        timeout && clearTimeout(timeout);
+      }
+    };
   }
 };
